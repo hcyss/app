7a3c0bd3f2a90860a624bbc4ff401d3abfaf325e
hlw <hlw@ncweal.com>
2022-10-19 17:22:11 +0800

    style(index):优化代码

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,16 +1,7 @@
 <template>
 	<view class="index">
-	<u--text mode="name" text="张三ss三" format="encrypt"></u--text>
-	<u--text mode="link" text="Go to uView docs" href="https://www.uviewui.com" ></u--text>
-	 <u-button text="月落"></u-button>
-	 <view class="">
-		 	<u-cell-group>
-		 		<u-cell icon="setting-fill" title="个人设置"></u-cell>
-		 		<u-cell icon="integral-fill" title="会员等级" value="新版本"></u-cell>
-		 	</u-cell-group>
-			
-	 </view>
-	 
+	<u--text mode="name" text="张三三" format="encrypt"></u--text>
+	
 	</view>
 	
 </template>
