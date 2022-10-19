db78a51ae700f775a7c00a43d19cbfe848bb5bd1
hlw <hlw@ncweal.com>
2022-10-19 17:24:20 +0800

    feat(components*):引入uView UI开发框架

---
+++ pages/page_code/page-specifics/page-specifics.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<u-button text="月落"></u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
